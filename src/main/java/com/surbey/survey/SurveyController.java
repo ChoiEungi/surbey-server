f58ec97336a287aa7e4537e514f6ae3dd39a49f0
grace_goose <choieungi@gm.gist.ac.kr>
2022-05-20 15:03:25 +0900

    feat: SurveyController 구현

--- src/main/java/com/surbey/survey/SurveyController.java
+++ src/main/java/com/surbey/survey/SurveyController.java
@@ -1,12 +1,37 @@
 package com.surbey.survey;
 
+import com.surbey.survey.dto.SurveyRequest;
+import com.surbey.survey.dto.SurveyResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/surveys")
 public class SurveyController {
 
+    private final SurveyServices surveyServices;
+
+    @PostMapping("")
+    public ResponseEntity<Void> createSurvey(@RequestBody SurveyRequest request){
+        UUID survey = surveyServices.createSurvey(request);
+        return ResponseEntity.created(URI.create("/surveys" + survey)).build();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<SurveyResponse>> retrieveSurveyList(){
+        List<SurveyResponse> surveyList = surveyServices.findSurveyList();
+        return ResponseEntity.ok(surveyList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SurveyResponse> retrieveSurvey(@PathVariable UUID id){
+        SurveyResponse survey = surveyServices.findSurveyById(id);
+        return ResponseEntity.ok(survey);
+    }
 }
