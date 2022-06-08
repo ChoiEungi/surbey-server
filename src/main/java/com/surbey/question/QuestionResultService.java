9a7bc9c8a0a67def53643b0d58e3b6c4c13dadf5
grace_goose <choieungi@gm.gist.ac.kr>
2022-06-08 19:45:20 +0900

    fix: request url 수정

--- src/main/java/com/surbey/question/QuestionResultService.java
+++ src/main/java/com/surbey/question/QuestionResultService.java
@@ -3,12 +3,15 @@ package com.surbey.question;
 import com.surbey.answer.Answer;
 import com.surbey.answer.AnswerRepository;
 import com.surbey.result.ResultRequest;
+import com.surbey.survey.Survey;
 import com.surbey.survey.SurveyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,8 +22,10 @@ public class QuestionResultService {
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
 
-    public void findResultBySurvey() {
-//        surveyRepository.
+    @Transactional(readOnly = true)
+    public void findResultBySurvey(UUID id) {
+        Survey survey = surveyRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        return;
     }
 
 
---
+++ src/main/java/com/surbey/question/dto/ResultResponse.java
@@ -0,0 +1,10 @@
+package com.surbey.question.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ResultResponse {
+
+}
